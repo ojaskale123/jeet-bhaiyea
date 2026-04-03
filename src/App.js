33168b5f204743b74207d5cadedb3e2441027254
ojas kale <ojask68@gmail.com>
2026-04-03 21:07:32 +0530

    fixed gitignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useMemo } from 'react';
-import '@/App.css';
+import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { initializeData, getCurrentUser } from './utils/localStorage';
